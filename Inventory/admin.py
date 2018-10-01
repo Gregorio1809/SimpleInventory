1b40e96155c79ad01db9b3e2bf9577fad7fb7719
Gregorio1809 <gregorioimanuel@gmail.com>
2018-10-01 16:29:58 +0700

    [UPDATE] adding fields in item model

--- Inventory/admin.py
+++ Inventory/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Item,Category,Client,Transaction
+from .models import Item,Category,Client,Transaction, Value
 
 admin.site.register(Item)
 admin.site.register(Client)
 admin.site.register(Category)
 admin.site.register(Transaction)
+admin.site.register(Value)
--- Inventory/models.py
+++ Inventory/models.py
@@ -1,33 +1,47 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Category(models.Model):
-	category=models.CharField(max_length=50)
-	def __str__(self):
-		return self.category		
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
+
+class Value(models.Model):
+    value = models.CharField(max_length=20, default='Kilograms')
+
+    def __str__(self):
+        return self.value
+
 
 class Item(models.Model):
-	item_id=models.AutoField(primary_key=True)
-	name=models.CharField(max_length=50)
-	category=models.ForeignKey(Category,on_delete=models.CASCADE)
-	quantity=models.IntegerField(default=0)
-	def __str__(self):
-		return self.name
+    item_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    value = models.ForeignKey(Value, on_delete=models.CASCADE, blank=True, null=True)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
 
 class Client(models.Model):
-	place=models.CharField(max_length=50)
-	description=models.CharField(max_length=100)
-	def __str__(self):
-		return self.place
-	
+    place = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.place
+
+
 class Transaction(models.Model):
-	trans_id=models.AutoField(primary_key=True)	
-	quantity=models.IntegerField()
-	time=models.DateTimeField(auto_now=True)
-	item=models.ForeignKey(Item,on_delete=models.CASCADE,blank=False,null=False)
-	client=models.ForeignKey(Client,on_delete=models.CASCADE,blank=False,null=False)
-	def __str__(self):
-		return ("%s , %s , %s" % (self.item.name,self.client.place,self.time))
+    trans_id = models.AutoField(primary_key=True)
+    quantity = models.IntegerField()
+    time = models.DateTimeField(auto_now=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, blank=False, null=False)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=False, null=False)
 
-
+    def __str__(self):
+        return ("%s , %s , %s" % (self.item.name, self.client.place, self.time))
